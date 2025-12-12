b8c0c350d6f494926ac279d416d9032e678cbb59
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-12-12 12:33:04 +0000

    fixed path issue on app.js to display public folder

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const entryRouter = require('./routers/entries');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public/index.html")))
+app.use(express.static(path.join(__dirname, "..", "public")))
 
 app.use('/entries', entryRouter);
 
