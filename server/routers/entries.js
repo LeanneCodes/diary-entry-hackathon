d19b3c57e5059a9fa65a1e16521cad782125504c
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-12-12 12:50:02 +0000

    update routers for the 3 filtered routes

--- server/routers/entries.js
+++ server/routers/entries.js
@@ -3,6 +3,12 @@ const entryController = require('../controllers/entries')
 
 const entryRouter = Router();
 
+// More specific routes must come first
+entryRouter.get("/recent", entryController.getRecent);
+entryRouter.get("/date/:date", entryController.getByDate);
+entryRouter.get("/category/:category", entryController.getByCategory);
+
+// General routes
 entryRouter.get("/", entryController.index);
 entryRouter.get("/:name", entryController.show);
 entryRouter.post("/", entryController.create);
