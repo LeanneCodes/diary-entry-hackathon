d004b8f37dec919b58245c536f391aa57625336f
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-12-12 09:50:46 +0000

    add router, app and index.js files

--- server/routers/entries.js
+++ server/routers/entries.js
@@ -1,1 +1,12 @@
-<></>
+const { Router } = require('express');
+const entryController = require('../controllers/entries')
+
+const entryRouter = Router();
+
+entryRouter.get("/", entryController.index);
+entryRouter.get("/:name", entryController.show);
+entryRouter.post("/", entryController.create);
+entryRouter.patch("/:name", entryController.update);
+entryRouter.delete("/:name", entryController.destroy);
+
+module.exports = entryRouter;
