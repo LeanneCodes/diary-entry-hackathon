28a554daed98f64556b4d81389e79005f3de85bc
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-12-12 10:15:42 +0000

    add structure for entry model

--- server/models/Entry.js
+++ server/models/Entry.js
@@ -1,1 +1,29 @@
-<></>
+const db = require('../db/connect')
+
+class Entry {
+    constructor({}) {
+
+    }
+
+    static async getAll() {
+
+    }
+
+    static async getOneEntry() {
+
+    }
+
+    static async create() {
+
+    }
+
+    async update() {
+
+    }
+
+    async destroy() {
+        
+    }
+}
+
+module.exports = Entry;
